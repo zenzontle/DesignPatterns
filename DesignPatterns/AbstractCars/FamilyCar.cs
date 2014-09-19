27cfb0e218f895e9cb3d61b8782b5c8330b6af66
zenzontle <zenzontle@gmail.com>
2014-09-19 16:59:41 -0700

    Finished AbstractFactory pattern

---
+++ DesignPatterns/AbstractCars/FamilyCar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.AbstractFactory.AbstractCars
+{
+    abstract class FamilyCar
+    {
+        public abstract void RunSlow();
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DesignPatterns.AbstractFactory.ConcreteFactories;
 
-namespace DesignPatterns
+namespace DesignPatterns.AbstractFactory
 {
     class Program
     {
         static void Main(string[] args)
         {
+            FordCarFactory fordCarFactory = new FordCarFactory();
+            Driver user1 = new Driver(fordCarFactory);
+            user1.RunCars();
+
+            MercedesCarFactory mercedesCarFactory = new MercedesCarFactory();
+            Driver user2 = new Driver(mercedesCarFactory);
+            user2.RunCars();
+
             Console.ReadKey();
         }
     }
