5f97e996c6cc32e6c5890d7d6d598908693e7026
zenzontle <zenzontle@gmail.com>
2014-09-21 23:56:32 -0700

    Singleton pattern

---
+++ Singleton/ApplicationState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class ApplicationState
+    {
+        private static volatile ApplicationState _instance;
+        private static object _lock = new object();
+
+        public static ApplicationState GetState()
+        {
+            if (_instance == null)
+            { 
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ApplicationState();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string LoginId { get; set; }
+        public int MaxSize { get; set; }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationState applicationState = ApplicationState.GetState();
+            applicationState.LoginId = "kanasz";
+            applicationState.MaxSize = 1024;
+
+            ApplicationState applicationState2 = ApplicationState.GetState();
+
+            Console.WriteLine(applicationState2.LoginId);
+            Console.WriteLine(applicationState2.MaxSize);
+            Console.WriteLine(applicationState == applicationState2);
+
+            Console.ReadKey();
+        }
+    }
+}
